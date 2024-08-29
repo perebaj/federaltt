b8306f97db984d0251138c4924da32353655de3d
perebaj <perebaj@gmail.com>
2024-08-28 22:01:11 -0300

    :sparkles: xandeco tt

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import "./globals.css";
+// import { Inter as FontSans } from "next/font/google";
+import { Quantico as Font } from "next/font/google";
+import {
+  ClerkProvider,
+  // SignInButton,
+  // SignedIn,
+  // SignedOut,
+  // UserButton
+} from "@clerk/nextjs";
+// const inter = Inter({ subsets: ["latin"] });
+import { cn } from "@/lib/utils";
+
+const fontMono = Font({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "federaltt",
+  description: "Todos seus processos em um só lugar",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn(fontMono.className, "bg-white-950 antialiased")}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import xandeco from "../public/xande.png";
+import { SignIn } from "@clerk/nextjs";
+import { TwitterIcon } from "@/components/icons";
+
+export default function Component() {
+  return (
+    <div>
+      <header className="flex items-center justify-between bg-[#15202B] px-6 py-4">
+        <Link href="#" prefetch={false}>
+          <TwitterIcon className="mr-2 h-5 w-5" />
+        </Link>
+        <Link
+          href="/contact"
+          className="text-sm text-[#1DA1F2] hover:underline focus:outline-none"
+          prefetch={false}
+        >
+          Contact
+        </Link>
+      </header>
+      <div className="grid h-screen grid-cols-2 bg-[#15202B]">
+        <div className="flex items-center justify-center">
+          <Image src={xandeco} width={500} height={500} alt="xandeco" />
+        </div>
+        <div className="flex min-h-screen items-center justify-center">
+          <SignIn />
+        </div>
+      </div>
+    </div>
+  );
+}
